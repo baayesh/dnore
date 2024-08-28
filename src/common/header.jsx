71f4f6ac175e0f4e15358aab818383f2f985f88c
ayeshb <ayeshbamunuarachchi@gmail.com>
2024-08-28 15:04:58 +0530

    create header

---
+++ src/common/header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../styles/header.css";
+
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <div className="container">
+        <div className="header-content">
+          {/* <img src={logo} alt="logo" className="logo" /> */}
+          <nav className="nav">
+            <div className="nav-list">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+              <a href="/products" className="nav-link">
+                Our&nbsp;Products
+              </a>
+              <a href="/video" className="nav-link">
+                Our&nbsp;Services
+              </a>
+              <a href="/contact" className="nav-link">
+                Office&nbsp;Spaces
+              </a>
+              <a href="/contact" className="nav-link">
+                About&nbsp;Us
+              </a>
+              <a href="/contact" className="nav-link">
+                Contact&nbsp;Us
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
