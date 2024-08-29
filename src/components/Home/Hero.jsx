bb041445f3d35037303c4cdf767389e3f2047be0
ayeshb <ayeshbamunuarachchi@gmail.com>
2024-08-30 05:17:52 +0530

    improve hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import hero_img from '../../assets/Home/hero_img.png'
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-container ">
-        {/* <video className='hero__vid' src={HeroVid} autoPlay loop muted /> */}
-
-        <h1 className="hero-title">Delmore</h1>
-        <p className="hero-text">Quality-Ethics-Sustainability</p>
+        <div className="hero-row">
+          <div className="hero-text-col">
+            <h1 className="hero-title">Dnore</h1>
+            <p className="hero-text">All Services You Need, Under One Hood</p>
+          </div>
+          <img src={hero_img} alt="Hero Image" />
+        </div>
       </div>
     </section>
   );
